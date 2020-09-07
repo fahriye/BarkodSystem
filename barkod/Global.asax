9d3b059edba716288575472c27e37d5f93e6d945
Fahriye Sinem <sinem_baskann@hotmail.com>
2020-09-07 11:42:42 +0300

    Barkod Sistemi

---
+++ barkod/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="barkod.MvcApplication" Language="C#" %>
---
+++ barkod/Models/BarkodaGoreUrunGetir.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace barkod.Models
+{
+    public class BarkodaGoreUrunGetir
+    {
+        public string Barkod { get; set; }
+    }
+}
